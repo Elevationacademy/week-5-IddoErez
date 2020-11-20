435376eabc58e488b08c5f6f4e54c4a2bdb39dab
Ido Erez <idodoerez@gmail.com>
2020-11-20 18:38:24 +0200

    Finished CRUD must exercises

---
+++ Bootcamp/Practice/Week-5/CRUD-Router-Middleware/word-counter/server/routes/api.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+
+const wordCounter = {}
+
+router.get('/sanity', function (req, res) {
+    res.send("server up and running")
+})
+
+router.get('/word/: word', function (req, res) {
+    if (wordCounter[req.params.word]) {
+        res.send({ count: wordCounter[req.params.word] })
+    } else {
+        res.send({ count: 0 })
+    }
+})
+
+router.post('/word/:word', function (req, res) {
+    if (wordCounter[req.params.word]) {
+        wordCounter[req.params.word]++
+    } else {
+        wordCounter[req.params.word] = 1
+    }
+    res.send({ text: `Added ${req.params.word}`, currentCount: wordCounter[req.params.word] })
+})
+
+
+
+router.post('/word/:sentence', function (req, res) {
+    let numNewWords =0
+    let numOldWords = 0 
+    let sentence = req.params.sentence
+    const sentenceToArray = sentence.split(" ")
+    for (let word of sentenceToArray) {
+        if (wordCounter[word]) {
+            wordCounter[word]++
+            numOldWords++
+        }
+     else {
+         wordCounter[word] = 1 
+         numNewWords++
+     }
+    }
+    res.send({text: `Added ${numNewWords} words, ${numOldWords} already existed`, currentCount: -1})
+})
+
+module.exports = router
